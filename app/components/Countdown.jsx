1057e8ddc1f136df87ef516e573c372974946e08
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-06 14:34:42 -0700

    formatting

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -3,14 +3,37 @@ var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
-	getInitialState: function() {
-		return {count: 0};
+	getInitialState: function () {
+		return {
+			count: 0,
+			countdownStatus: 'stopped'
+		};
 	},
-	handleSetCountdown: function(seconds) {
-		this.setState({count: seconds});
+	componentDidUpdate: function (prevProps, prevState) {
+		if (this.state.countdownStatus !== prevState.countdownStatus) {
+			switch (this.state.countdownStatus) {
+			case 'started':
+				this.startTimer();
+				break;
+			}
+		}
 	},
-	render: function() {
-		var {count} = this.state;
+	startTimer: function () {
+		this.timer = setInterval(() => {
+			var newCount = this.state.count - 1;
+			this.setState({
+				count: newCount >= 0 ? newCount : 0
+			});
+		}, 1000);
+	},
+	handleSetCountdown: function (seconds) {
+		this.setState({
+			count: seconds,
+			countdownStatus: 'started'
+		});
+	},
+	render: function () {
+		var { count } = this.state;
 		return (
 			<div>
 				<Clock totalSeconds={count}/>
