9cd0ffe4ba58cba009ab65c8aa7e6f751fc8c0ff
Kyle Asaff <kyleasaff@pixleeteam.com>
2017-06-07 13:18:10 -0700

    webpack scss

--- app/app.jsx
+++ app/app.jsx
@@ -6,11 +6,11 @@ var Countdown = require('Countdown');
 var Timer = require('Timer');
 
 // Load foundation
-require('style!css!foundation-sites/dist/css/foundation.min.css')
+require('style!css!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
 
 // App css
-require('style!css!sass!applicationStyles')
+require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   <Router history={hashHistory}>
